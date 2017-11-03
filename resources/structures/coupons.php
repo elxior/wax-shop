6e7c92135a742458ebf6d55376ddf6402135eb2b
Duane Helton <tallcoder@gmail.com>
2017-11-03 11:44:13 -0400

    moved html snippet to a blade file

--- resources/structures/coupons.php
+++ resources/structures/coupons.php
@@ -7,12 +7,7 @@ $structure = [
     'table' => 'coupons',
     'primary_key' => 'id',
     'fields' => [],
-    'notes' =>
-                '<ul>'.
-                '<li style="display: inline; list-style-type: none; padding-right: 20px;"><a href="' . URL::to('admin/cms/coupons/bulk/generate') . '">&bull; Bulk <strong>Generate</strong> Coupon Codes</a></li>'.
-                '<li style="display: inline; list-style-type: none; padding-right: 20px;"><a href="' . URL::to('admin/cms/coupons/bulk/import') . '">&bull; Bulk <strong>Load</strong> Coupons From CSV</a></li>'.
-                '<li style="display: inline; list-style-type: none; padding-right: 20px;"><a href="' . URL::to('admin/cms/coupons/bulk/export') . '">&bull; Bulk <strong>Export</strong> Coupons To CSV</a></li>'.
-                '</ul>',
+    'notes' => view('shop::components.admin.coupons.structure_notes'),
     'settings' => [
         'page_title' => 'Coupons',
         'create_new_text' => 'Create New Coupon',
---
+++ resources/views/components/admin/coupons/structure_notes.blade.php
@@ -0,0 +1,5 @@
+<ul>
+    <li style="display: inline; list-style-type: none; padding-right: 20px;"><a href=" {{ URL::to('admin/cms/coupons/bulk/generate') }}">&bull; Bulk <strong>Generate</strong> Coupon Codes</a></li>
+    <li style="display: inline; list-style-type: none; padding-right: 20px;"><a href="{{ URL::to('admin/cms/coupons/bulk/import') }}">&bull; Bulk <strong>Load</strong> Coupons From CSV</a></li>
+    <li style="display: inline; list-style-type: none; padding-right: 20px;"><a href="{{ URL::to('admin/cms/coupons/bulk/export') }}">&bull; Bulk <strong>Export</strong> Coupons To CSV</a></li>
+</ul>
