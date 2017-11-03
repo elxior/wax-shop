582633cfb1d41308084cecbf4cc925e5d785a18c
Duane Helton <tallcoder@gmail.com>
2017-11-03 11:37:15 -0400

    standards update

--- src/Providers/ShopServiceProvider.php
+++ src/Providers/ShopServiceProvider.php
@@ -94,8 +94,9 @@ class ShopServiceProvider extends ServiceProvider
         $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../../database/factories');
 
         // add example CSV to project
+        $publicPath = public_path('res/uploads/shop/coupons/example_coupon_import.csv');
         $this->publishes([
-            __DIR__ . '/../../resources/coupons/example_coupon_import.csv' => public_path('res/uploads/shop/coupons/example_coupon_import.csv')
+            __DIR__ . '/../../resources/coupons/example_coupon_import.csv' => $publicPath
         ], 'public');
     }
 
