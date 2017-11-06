67859d04701260b3275a4fac00501cf92d3eb184
David Woodmansee <woody@oohology.com>
2017-11-06 14:08:20 -0500

    remove laravel-omnipay package again

---
+++ src/Drivers/Payment/AuthorizeNetCimDriver.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wax\Shop\Drivers\Payment;
+
+use Omnipay\Omnipay;
+
+class AuthorizeNetCimDriver
+{
+    protected $gateway;
+
+    public function __construct()
+    {
+        //$this->gateway = Omnipay::
+    }
+
+}
