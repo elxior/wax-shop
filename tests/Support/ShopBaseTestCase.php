5e6ec68e8e2f56864689073f1204a5e852bf6936
David Woodmansee <woody@oohology.com>
2017-11-22 17:21:11 -0500

    bind ShopServiceProvider before PagesServiceProvider

--- tests/Support/ShopBaseTestCase.php
+++ tests/Support/ShopBaseTestCase.php
@@ -2,24 +2,31 @@
 
 namespace Tests\Shop\Support;
 
+use Illuminate\Foundation\Bootstrap\RegisterProviders;
 use Tests\WaxAppTestCase;
+use Wax\Pages\Providers\PagesServiceProvider;
 use Wax\Shop\Providers\ShopServiceProvider;
 
 class ShopBaseTestCase extends WaxAppTestCase
 {
     public function setUp()
     {
+        // Make sure ShopServiceProvider is before PagesServiceProvider in the list BEFORE the providers boot
+        $this->registerBeforeBootstrappingCallback(RegisterProviders::class, function() {
+            $providers = config('app.providers');
+
+            if(false !== ($offset = array_search(PagesServiceProvider::class, $providers))) {
+                array_splice($providers, $offset, 0, ShopServiceProvider::class);
+            } else {
+                $providers[] = ShopServiceProvider::class;
+            }
+
+            config(['app.providers' => $providers]);
+        });
+
         parent::setUp();
-
-        $this->app->register(ShopServiceProvider::class);
-
-        \Route::getRoutes()->refreshNameLookups();
 
         $migrator = $this->app->make('migrator');
         $migrator->run(__DIR__ . '/../../database/migrations');
-
-        // it would be nice if wax-app ran these automatically.
-        $migrator->run(__DIR__ . '/../../vendor/oohology/wax-cms/modules/core/database/migrations');
-        //$migrator->run(__DIR__.'/../../vendor/oohology/wax-cms/modules/pages/database/migrations');
     }
 }
