33b2a986578016238b83afefae2eb404b773ae58
David Woodmansee <woody@oohology.com>
2017-12-08 18:03:27 -0500

    LoginListener tweaks, add some test cases

--- src/Listeners/LoginListener.php
+++ src/Listeners/LoginListener.php
@@ -26,26 +26,36 @@ class LoginListener
     {
         $this->deleteSavedIncompleteOrdersWithEmptyCart($event);
 
-        $activeOrder = Order::where('session_id', session()->getId())->first();
-
+        $activeOrder = $this->orderRepo->getOrderModel()->active()->where('session_id', session()->getId())->first();
         if ($activeOrder) {
-            if (($activeOrder->item_count == 0) && ($this->getSavedOrders($event)->count() > 0)) {
-                $activeOrder->delete();
-                return true;
-            }
-
-            if ($activeOrder->item_count > 0) {
-                $this->getSavedOrders($event)
-                    ->each
-                    ->delete();
-            }
-
-            $activeOrder->session_id = null;
-            $activeOrder->user_id = $event->user->id;
-            $activeOrder->save();
-
-            $this->setOrderDefaultAddresses($activeOrder, $event->user);
+            $this->handleActiveOrder($activeOrder, $event);
         }
+
+        // switch all orders matching the session id to have a user_id instead
+        $this->orderRepo->getOrderModel()->where('session_id', session()->getId())
+            ->each(function ($order) use ($event) {
+                $order->session_id = null;
+                $order->user_id = $event->user->id;
+                $order->save();
+            });
+    }
+
+    protected function handleActiveOrder(Order $activeOrder, Login $event)
+    {
+        // if the session cart is empty but the user has saved order(s), delete the session order
+        if (($activeOrder->item_count == 0) && ($this->getSavedOrders($event)->count() > 0)) {
+            $activeOrder->delete();
+            return;
+        }
+
+        // if the session cart has items, it replaces any saved incomplete orders
+        if ($activeOrder->item_count > 0) {
+            $this->getSavedOrders($event)
+                ->each
+                ->delete();
+        }
+
+        $this->setOrderDefaultAddresses($activeOrder, $event->user);
     }
 
     protected function getSavedOrders(Login $event)
--- tests/Support/ShopBaseTestCase.php
+++ tests/Support/ShopBaseTestCase.php
@@ -4,18 +4,22 @@ namespace Tests\Shop\Support;
 
 use Illuminate\Foundation\Bootstrap\RegisterProviders;
 use Tests\WaxAppTestCase;
+use Wax\Core\Support\ConfigurationDatabase;
 use Wax\Pages\Providers\PagesServiceProvider;
 use Wax\Shop\Providers\ShopServiceProvider;
 
 class ShopBaseTestCase extends WaxAppTestCase
 {
+    protected $testMailFrom = 'noreply@example.org';
+    protected $testMailTo = 'test1@example.org, test2@example.org';
+
     public function setUp()
     {
         // Make sure ShopServiceProvider is before PagesServiceProvider in the list BEFORE the providers boot
-        $this->registerBeforeBootstrappingCallback(RegisterProviders::class, function() {
+        $this->registerBeforeBootstrappingCallback(RegisterProviders::class, function () {
             $providers = config('app.providers');
 
-            if(false !== ($offset = array_search(PagesServiceProvider::class, $providers))) {
+            if (false !== ($offset = array_search(PagesServiceProvider::class, $providers))) {
                 array_splice($providers, $offset, 0, ShopServiceProvider::class);
             } else {
                 $providers[] = ShopServiceProvider::class;
@@ -28,5 +32,18 @@ class ShopBaseTestCase extends WaxAppTestCase
 
         $migrator = $this->app->make('migrator');
         $migrator->run(__DIR__ . '/../../database/migrations');
+
+        app()->bind(ConfigurationDatabase::class, function () {
+            $double = \Mockery::mock(ConfigurationDatabase::class);
+            $double->shouldReceive('get')
+                ->with('WEBSITE_MAILFROM')
+                ->andReturn($this->testMailFrom);
+
+            $double->shouldReceive('get')
+                ->with('WEBSITE_MAILTO')
+                ->andReturn($this->testMailTo);
+
+            return $double;
+        });
     }
 }
