da8a0551cb6d1356486b4e9695718080b4376b41
David Bankes <davidbankes@gmail.com>
2017-12-12 17:23:24 -0500

    better logic

--- src/Listeners/OrderPlaced/InventoryListener.php
+++ src/Listeners/OrderPlaced/InventoryListener.php
@@ -8,16 +8,18 @@ class InventoryListener
 {
     public function handle(OrderPlacedEvent $event)
     {
-        if (config('wax.shop.inventory.track')) {
-            $event->order()->items->each(function ($item) {
-                if ($item->modifier && !is_null($item->modifier->getAttributes()['inventory'])) {
-                    $item->modifier->inventory -= $item->quantity;
-                    $item->modifier->save();
-                } else {
-                    $item->product->inventory -= $item->quantity;
-                    $item->product->save();
-                }
-            });
+        if (!config('wax.shop.inventory.track')) {
+            return;
         }
+
+        $event->order()->items->each(function ($item) {
+            if ($item->modifier && !is_null($item->modifier->getAttributes()['inventory'])) {
+                $item->modifier->inventory -= $item->quantity;
+                $item->modifier->save();
+            } else {
+                $item->product->inventory -= $item->quantity;
+                $item->product->save();
+            }
+        });
     }
 }
