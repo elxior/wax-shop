b2bfdfa29ea08ac69dac9a815d3e52477684c3b5
David Bankes <davidbankes@gmail.com>
2018-04-19 16:57:16 -0400

    fix for deleting items from non-current order

--- src/Validators/DeleteOrderItemValidator.php
+++ src/Validators/DeleteOrderItemValidator.php
@@ -3,24 +3,26 @@
 namespace Wax\Shop\Validators;
 
 use Wax\Shop\Facades\ShopServiceFacade;
+use Wax\Shop\Models\Order;
 use Wax\Shop\Models\Order\Item;
 use Illuminate\Support\MessageBag;
 
 class DeleteOrderItemValidator extends AbstractValidator
 {
     protected $itemId;
-    protected $quantity;
+    protected $order;
 
-    public function __construct(int $itemId)
+    public function __construct(int $itemId, Order $order)
     {
         $this->itemId = $itemId;
+        $this->order = $order;
     }
 
     public function passes() : bool
     {
         $this->messages = new MessageBag;
 
-        $item = ShopServiceFacade::getActiveOrder()->items->where('id', $this->itemId)->first();
+        $item = $this->order->items->where('id', $this->itemId)->first();
         if (!$item) {
             $this->errors()->add('item_id', 'Invalid Order Item');
             return false;
