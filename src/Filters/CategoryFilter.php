03c476afc29dc4239d81e040adb084510c0fb8bc
dbankes <davidbankes@gmail.com>
2020-08-27 18:30:38 -0400

    compatibility newer filter system

--- src/Filters/CategoryFilter.php
+++ src/Filters/CategoryFilter.php
@@ -4,34 +4,15 @@ namespace Wax\Shop\Filters;
 
 use Wax\Shop\Models\Product;
 use Wax\Shop\Models\Product\Category;
-use Illuminate\Database\Eloquent\Model;
-use Wax\Core\Filters\Filter;
-use Wax\Core\Filters\FilterOption;
+use Wax\Core\Filters\FilterRelation;
 
-class CategoryFilter extends Filter
+class CategoryFilter extends FilterRelation
 {
     protected $model = Category::class;
     protected $baseModel = Product::class;
     protected $relation = 'category';
-    protected $inverseRelation = 'articles';
-    protected $comparisonColumn = 'url_slug';
+    protected $inverseRelation = 'products';
+    protected $whereColumn = 'url_slug';
+    protected $labelColumn = 'name';
     protected $name = 'category';
-
-    public function accepts(Model $model)
-    {
-        return $model->categories ? $model->categories->pluck('url_slug')->contains($this->getValue()) : false;
-    }
-
-    public function extractModelLabel(Model $model)
-    {
-        return $model->name;
-    }
-
-    public function buildOption(Model $model)
-    {
-        $value = $this->extractModelValue($model);
-        $label = $this->extractModelLabel($model);
-
-        return new FilterOption($label, $value);
-    }
 }
